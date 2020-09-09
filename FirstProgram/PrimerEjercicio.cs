294f2c3389c44c607a83878213cd8481ecb3d26f
Agner <agnervillafabrega@gmail.com>
2020-09-09 12:21:07 -0500

    Agregar archivos de proyecto.

---
+++ FirstProgram/PrimerEjercicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double promedio;
+            double primeraNota;
+            double segundaNota;
+            float terceraNota ;
+
+            Console.Write("NOTA 1: ");
+            primeraNota = Convert.ToDouble(Console.ReadLine());
+            Console.Write("NOTA 2: ");
+            segundaNota = double.Parse(Console.ReadLine());
+            Console.Write("NOTA 3: ");
+            terceraNota = float.Parse(Console.ReadLine());
+
+            promedio = (primeraNota + segundaNota + terceraNota) / 3 ;
+
+            Console.WriteLine("Promedio: {0}", promedio);
+
+            if (promedio>= 3.0){
+                Console.WriteLine("FELICITACIONES APROBASTE LA MATERIA");
+            }
+            else{
+                Console.WriteLine("MATERIA REPROBADA");
+            }
+            Console.ReadKey();
+        }
+    }
+}
